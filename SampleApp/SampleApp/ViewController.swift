460dfa28bb5c3efe8b0d8c505fd23b0d73a2d0fd
Rohan Sanap <sanaprohan10@gmail.com>
2020-11-04 11:00:51 +0530

    Add timer, asset reader

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -20,6 +20,19 @@ class ViewController: UIViewController {
   let asset = AVAsset(url: Bundle.main.url(forResource: "football", withExtension: "mp4")!)
   
   lazy var assetReader: AVAssetReader = try! AVAssetReader(asset: asset)
+  
+  let outputSettings: [String: Any] = [
+    kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA,
+    kCVPixelBufferMetalCompatibilityKey as String: true
+  ]
+  
+  lazy var trackOutput: AVAssetReaderTrackOutput = {
+    let track = asset.tracks(withMediaType: .video).first!
+    let trackOutput = AVAssetReaderTrackOutput(track: track, outputSettings: outputSettings)
+    return trackOutput
+  }()
+  
+  var timer: Timer?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -34,6 +47,26 @@ class ViewController: UIViewController {
     
     addChild(korgeVC)
     korgeVC.didMove(toParent: self)
+    
+    assetReader.add(trackOutput)
+    assetReader.startReading()
+  }
+  
+  var counter = 0
+  @objc func timerInvocation(_ sender: Timer) {
+    guard let sampleBuffer = trackOutput.copyNextSampleBuffer() else { return }
+  }
+  
+  @IBAction func buttonTapped(_ sender: UIButton) {
+    if let timer = timer, timer.isValid {
+      timer.invalidate()
+    }else {
+      self.timer = Timer.scheduledTimer(timeInterval: 0.0416666,
+                                        target: self,
+                                        selector: #selector(timerInvocation),
+                                        userInfo: nil,
+                                        repeats: true)
+    }
   }
 
 
